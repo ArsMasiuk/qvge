d7a7e3b9f49d42af07b3e470b116c2ab9549bdfc
Ars <ars.masiuk@gmail.com>
2021-02-14 14:26:51 +0100

    #141 merged back

--- src/qvgeio/CFormatPlainDOT.h
+++ src/qvgeio/CFormatPlainDOT.h
@@ -2,7 +2,7 @@
 This file is a part of
 QVGE - Qt Visual Graph Editor
 
-(c) 2016-2020 Ars L. Masiuk (ars.masiuk@gmail.com)
+(c) 2016-2021 Ars L. Masiuk (ars.masiuk@gmail.com)
 
 It can be used freely, maintaining the information above.
 */
@@ -28,9 +28,9 @@ private:
 		Graph* g = nullptr;
 	};
 
-	bool parseGraph(QString& line, const QVector<QStringRef>& refs, GraphInternal &gi) const;
-	bool parseNode(QString& line, const QVector<QStringRef>& refs, GraphInternal &gi) const;
-	bool parseEdge(QString& line, const QVector<QStringRef>& refs, GraphInternal &gi) const;
+	bool parseGraph(const QStringList& refs, GraphInternal &gi) const;
+	bool parseNode(const QStringList& refs, GraphInternal &gi) const;
+	bool parseEdge(const QStringList& refs, GraphInternal &gi) const;
 };
 
 
