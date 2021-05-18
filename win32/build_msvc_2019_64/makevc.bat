7240bb9c687dfe5b7277d421823137f8ce1059bf
Ars Masiuk <ars.masiuk@gmail.com>
2021-05-18 20:13:39 +0200

    latest fixes

---
+++ win32/build_msvc_2019_64/makevc.bat
@@ -0,0 +1,3 @@
+call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" 
+
+c:\Qt5.7\5.15.1\msvc2019_64\bin\qmake -r ../../src/qvge.pro -tp vc -recursive
