0d1fb4ffb8a8eabb8fa1e803a7dac4ed661c521f
Ars Masiuk <ars.masiuk@gmail.com>
2024-03-24 12:14:57 +0100

    script vs2022

--- win32/build_msvc_2022_64/deployvc.bat
+++ win32/build_msvc_2022_64/deployvc.bat
@@ -1,3 +1,1 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat" 
-
-c:\Qt\5.15.12\msvc2022_64\bin\qmake -r ../../src/qvge.pro -tp vc -recursive
+c:\Qt\5.15.12\msvc2022_64\bin\windeployqt ./bin/qvge.exe
